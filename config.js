660c7dbffdb8d1e69652357b81184103e0b48208
jccandela <jccandelabordera@gmail.com>
2021-01-11 19:16:35 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,4 @@
 module.exports = {
     'secret': 'Candela',
-    //remote
-    'database': 'mongodb://bordera:prueba@ds121575.mlab.com:21575/adi'
-    //local
-    //'database': 'mongodb://localhost/db_name'
+    'database': 'mongodb://localhost/db_name'
 }
